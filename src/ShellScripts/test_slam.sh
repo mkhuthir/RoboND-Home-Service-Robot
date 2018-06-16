957c0d76673bf32085d5461e03913638b7f41520
mkhuthir <mkhuthir@gmail.com>
2018-06-16 20:54:25 +0400

    progs: finished test_slam

--- src/ShellScripts/test_slam.sh
+++ src/ShellScripts/test_slam.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-xterm -e "export TURTLEBOT_GAZEBO_WORLD_FILE="~/catkin_ws/src/World/u_shape.world"" &
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch"
+
+# Deploy Turtlebot in personal environment
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$HOME/catkin_ws/src/World/personal.world" &
+sleep 5
+
+# Run slam_gmapping to perfrom SLAM
+xterm -e "rosrun gmapping slam_gmapping" &
+sleep 5
+
+# view_navigation to observe the map in rviz
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
+
+# keyboard_teleop to manually control the robot with keyboard commands
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
