f86db1113890a636e114a217dfa13360f5046e13
mkhuthir <mkhuthir@gmail.com>
2018-06-20 23:43:01 +0400

    progs: updated navi script

--- src/ShellScripts/test_navigation.sh
+++ src/ShellScripts/test_navigation.sh
@@ -5,7 +5,7 @@ xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$HOME/ca
 sleep 5
 
 # amcl_demo to loclize the robot
-xterm -e "" &
+xterm -e "roslaunch turtlebot_navigation amcl_demo.launch" &
 sleep 5
 
 # view_navigation to observe the map in rviz
