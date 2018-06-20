360a42ca5818cd1f36a332067cc89641dc7edde4
mkhuthir <mkhuthir@gmail.com>
2018-06-20 23:38:00 +0400

    progs: updated navigation script

--- src/ShellScripts/test_navigation.sh
+++ src/ShellScripts/test_navigation.sh
@@ -1,1 +1,13 @@
 #!/bin/sh
+
+# Deploy Turtlebot in personal environment
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$HOME/catkin_ws/src/World/personal.world" &
+sleep 5
+
+# amcl_demo to loclize the robot
+xterm -e "" &
+sleep 5
+
+# view_navigation to observe the map in rviz
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
