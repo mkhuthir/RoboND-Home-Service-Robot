381ffe59f0d54a2fb0c67055f825c37ae9937ab8
mkhuthir <mkhuthir@gmail.com>
2018-06-23 22:21:44 +0400

    progs: updated add_markers

--- src/add_markers/src/add_markers_node.cpp
+++ src/add_markers/src/add_markers_node.cpp
@@ -29,7 +29,7 @@ int main( int argc, char** argv )
     marker.action = visualization_msgs::Marker::ADD;
 
     // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-    marker.pose.position.x = 0;
+    marker.pose.position.x = 2;
     marker.pose.position.y = 0;
     marker.pose.position.z = 0;
     marker.pose.orientation.x = 0.0;
@@ -38,15 +38,15 @@ int main( int argc, char** argv )
     marker.pose.orientation.w = 1.0;
 
     // Set the scale of the marker -- 1x1x1 here means 1m on a side
-    marker.scale.x = 1.0;
-    marker.scale.y = 1.0;
-    marker.scale.z = 1.0;
+    marker.scale.x = 0.25;
+    marker.scale.y = 0.25;
+    marker.scale.z = 0.25;
 
     // Set the color -- be sure to set alpha to something non-zero!
     marker.color.r = 0.0f;
     marker.color.g = 1.0f;
     marker.color.b = 0.0f;
-    marker.color.a = 1.0;
+    marker.color.a = 1.0f;
 
     marker.lifetime = ros::Duration();
 
@@ -60,6 +60,28 @@ int main( int argc, char** argv )
       ROS_WARN_ONCE("Please create a subscriber to the marker");
       sleep(1);
     }
+    marker_pub.publish(marker);
+
+    // Wait 5 Sec
+    ros::Duration(5.0).sleep();
+
+    // Hide the marker
+    marker.action = visualization_msgs::Marker::DELETE;
+    
+    marker_pub.publish(marker);
+
+    // Wait 5 Sec
+    ros::Duration(5.0).sleep();
+
+
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    marker.pose.position.x = 3;
+    marker.pose.position.y = 1;
+    marker.pose.position.z = 0;
+
+    // Show the marker
+    marker.action = visualization_msgs::Marker::ADD;
+    
     marker_pub.publish(marker);
 
     r.sleep();
